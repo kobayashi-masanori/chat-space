9ad7fff411a6aa03cda852ddc53fa0a17840186b
kobayashi-masanori <68869343+kobayashi-masanori@users.noreply.github.com>
2020-08-16 13:11:13 +0900

    グループ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
     else
       render :edit
     end
- end
+  end
 
- private
+  private
 
- def user_params
-   params.require(:user).permit(:name, :email)
- end
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
